5ad195a6f0309fc9320042ea905d46ef3d0ef0f3
Billal Hossen <178034587+veloralbillal@users.noreply.github.com>
2026-08-11 21:53:01 +0600

    Fbhrj

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import expressApp from './server.js';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: process.env.BASE_PATH || (process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/Bio/'),
     plugins: [
       react(),
       tailwindcss(),
